5d4fe3bfc0b418d3f844f0babface222ddfc2a5e
KrasnopyorovN <nikita.19861@gmail.com>
2020-07-17 17:16:11 +0700

    Add global method security

--- menu/src/main/java/menu/MenuApplication.java
+++ menu/src/main/java/menu/MenuApplication.java
@@ -2,8 +2,14 @@ package menu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
 public class MenuApplication {
     public static void main(String[] args) {
         SpringApplication.run(MenuApplication.class, args);
